53b5143a599880e9b0e963b80f63ee521fb1266d
董舜尧 <dongshunyao@qq.com>
2018-11-25 22:41:02 +0800

    rf.v代码格式修改

--- rf.v
+++ rf.v
@@ -1,27 +1,30 @@
-module rf(busW,RegWr,RW,RA,RB,busA,busB,clock,reset);
-	input clock,reset,RegWr;
-	input [4:0] RW,RA,RB;
+module rf(busW, RegWr, RW, RA, RB, busA, busB, clock, reset);
+	input clock, reset, RegWr;
+	input [4:0] RW, RA, RB;
 	input [31:0] busW;
 
-	output [31:0] busA,busB;
+	output [31:0] busA, busB;
 
 	reg [31:0] register[31:0];
+
 	integer i;
 
-	assign busA=register[RB];
-	assign busB=register[RB];
+	assign busA = register[RB];
+	assign busB = register[RB];
+
 
 	initial
 	begin
-		for (i=0;i<32;i=i+1) register[i]=0;
+		for (i = 0; i < 32; i = i + 1) register[i] = 0;
 	end
 
-	always@(posedge clock,posedge reset)
+
+	always@(posedge clock, posedge reset)
 	begin
 		if (reset)
-			for (i=0;i<32;i=i+1) register[i]=0;
+			for (i = 0; i < 32; i = i + 1) register[i] = 0;
 		else
-			if (RegWr && RW!=0) register[RW]=busW;
+			if (RegWr && RW != 0) register[RW] = busW;
 	end
 
 endmodule
