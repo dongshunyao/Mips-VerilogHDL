f7db7ccb40c1807da7c955d587559f7894ea7069
董舜尧 <dongshunyao@qq.com>
2018-11-27 15:16:15 +0800

    修改im.v添加reset

--- im.v
+++ im.v
@@ -1,16 +1,24 @@
-module im(Addr, out);
+module im(Addr, out, reset);
+	input reset;
 	input [9:0] Addr;
 
 	output [31:0] out;
 
 	reg [31:0] memory[1023:0];
 
+	integer i;
 
 	assign out = memory[Addr];
 
 
 	initial
 	begin
+		$readmemh("code.txt", memory);
+	end
+
+	always@(posedge reset)
+	begin
+		for (i = 0; i < 1024; i = i + 1) memory[i] = 0;
 		$readmemh("code.txt", memory);
 	end
 
