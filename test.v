d9161ecb1f403759afc6d2c2501b398aedb3cb29
董舜尧 <dongshunyao@qq.com>
2018-11-27 22:47:36 +0800

    修复所有bug，指令lw，sw，beq，j测试通过

--- test.v
+++ test.v
@@ -4,11 +4,11 @@ module test();
 	initial
 	begin
 		clock = 0;
-		#5 reset = 1;
+		reset = 1;
         #5 reset = 0;
 	end
 
-	always #20 clock = ~clock;
+	always #10 clock = ~clock;
 
 	mips mips(.clock(clock), .reset(reset));
 
