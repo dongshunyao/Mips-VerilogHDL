5879d42f3d241aba1f429cd60b891d3e99f02948
董舜尧 <dongshunyao@qq.com>
2018-11-27 14:50:21 +0800

    修改rf.v增加RW的选择器

--- rf.v
+++ rf.v
@@ -1,15 +1,18 @@
-module rf(busW, RegWr, RW, RA, RB, busA, busB, clock, reset);
-	input clock, reset, RegWr;
-	input [4:0] RW, RA, RB;
+module rf(busW, RegWr, RegDst, rd, rt, RA, RB, busA, busB, clock, reset);
+	input clock, reset, RegWr, RegDst;
+	input [4:0] rd, rt, RA, RB;
 	input [31:0] busW;
 
 	output [31:0] busA, busB;
 
 	reg [31:0] register[31:0];
 
+	wire [4:0] RW;
+
 	integer i;
 
 
+	assign RW = RegDst ? rd : rt;
 	assign busA = register[RB];
 	assign busB = register[RB];
 
